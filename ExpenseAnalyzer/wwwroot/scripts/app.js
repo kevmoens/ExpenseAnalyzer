4f99af4eb7d313f20a8eba8a13801874ad36705f
Kevin Moens <kevmoens@yahoo.com>
2022-06-04 23:02:00 -0500

    fix javascript call

--- ExpenseAnalyzer/wwwroot/scripts/app.js
+++ ExpenseAnalyzer/wwwroot/scripts/app.js
@@ -1,10 +1,7 @@
-﻿
-export function getSelectionText() {
+﻿ function getSelectionText() {
     var text = "";
     if (window.getSelection) {
         text = window.getSelection().toString();
-            < !--} else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text; -->
             }
     return text;
 }
