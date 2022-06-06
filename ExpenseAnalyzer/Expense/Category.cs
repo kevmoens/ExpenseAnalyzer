37790d26f61bcd1292c9d10ea059757a5f014b4d
Kevin Moens <kevmoens@yahoo.com>
2022-06-05 20:35:25 -0500

    Add Modal window

---
+++ ExpenseAnalyzer/Expense/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ExpenseAnalyzer.Expense
+{
+	public class Category
+	{
+		public Category()
+		{
+		}
+        public bool? Selected { get; set; }
+        public string? Name { get; set; }
+		public List<string> Conditions { get; set; } = new List<string>();
+    }
+}
+
--- ExpenseAnalyzer/Program.cs
+++ ExpenseAnalyzer/Program.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+﻿using Blazored.Modal;
 using ExpenseAnalyzer;
-using MudBlazor.Services;
 using ExpenseAnalyzer.Expense;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,6 +13,7 @@ builder.Services.AddMudServices();
 builder.Services.AddSingleton<List<Transaction>>();
 builder.Services.AddTransient<Transaction>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredModal();
 
 await builder.Build().RunAsync();
 
